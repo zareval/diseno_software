10488a32c6e21db30826cc706dc97221db52009e
Zare <zarellaburbanov@gmail.com>
2023-09-28 09:25:50 -0500

    Agregando invoiceItem

---
+++ InvoiceItem/src/InvoiceItem/Pkg/EjercicioInvoiceItem.java
@@ -0,0 +1,10 @@
+package InvoiceItem.Pkg;
+
+public class EjercicioInvoiceItem {
+	public static void main(String[] args) {
+		InvoiceItem miInvoiceItem = new InvoiceItem("123123123","5%",10,5000);
+		
+		System.out.println("Name Product: Ajinomen");
+		System.out.println(miInvoiceItem);
+	}
+}
